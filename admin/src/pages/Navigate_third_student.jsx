9c6a7f33024c01a94a4bb0ac52de806abd2799bf
unknown <sharifrayan952@gmail.com>
2025-12-06 03:07:23 +0530

    Successfull

--- admin/src/pages/Navigate_third_student.jsx
+++ admin/src/pages/Navigate_third_student.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Classes from "../components/Classes";
 import { useParams } from "react-router-dom";
-import Thirdyear from "../components/thirdyear";
+import Thirdyear from "../components/Thirdyear";
 
 const DashboardContainer = styled.div`
   // text-align: center;
